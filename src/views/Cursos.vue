2346799417f573443e1f2b4c66c374131b761622
Pedro Luiz <p3dr0c4@live.com>
2020-04-04 20:53:06 -0300

    Contato2

--- src/views/Cursos.vue
+++ src/views/Cursos.vue
@@ -5,8 +5,16 @@
     </div>
     <transition>
       <div v-if="api">
-        <h1>Contato</h1>
-        {{ api }}
+        <h1>{{api.titulo}}</h1>
+        <p>{{api.descricao}}</p>
+        <ul>
+          <li v-for="curso in api.cursos" :key="curso.id">
+            <h2>
+              <router-link :to="{name: 'curso', params: {curso: curso.id}}">{{curso.nome}} - {{curso.totalAulas}} aulas | {{curso.horas}} horas</router-link>
+            </h2>
+            <p>{{curso.descricao}}</p>
+          </li>
+        </ul>
       </div>
     </transition>
   </div>
@@ -23,4 +31,8 @@ export default {
   }
 };
 </script>
-<style></style>
+<style scoped>
+  ul {
+    list-style: none;
+  }
+</style>
